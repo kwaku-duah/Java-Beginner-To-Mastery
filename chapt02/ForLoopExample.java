89e9d5910004fbe2c75b71f0bd505603989617f4
Sheikhman_Kalderon <duah229@outlook.com>
2025-05-05 23:14:16 +0000

    feature: example file for loop

---
+++ chapt02/ForLoopExample.java
@@ -0,0 +1,9 @@
+package chapt02;
+
+public class ForLoopExample {
+    //Demonstrate the for loop in java
+    public static void main(String[] args) {
+        int x;
+        for (x = 0; x < 10; x = x + 1 ) System.out.println("This is x " + x);
+    }
+}
