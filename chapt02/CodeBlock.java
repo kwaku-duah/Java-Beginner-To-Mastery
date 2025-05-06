4766130d2817f84a9a171879941428833a1459de
Sheikhman_Kalderon <duah229@outlook.com>
2025-05-06 08:49:24 +0000

    chore: add code blocks structure

---
+++ chapt02/CodeBlock.java
@@ -0,0 +1,13 @@
+package chapt02;
+
+public class CodeBlock {
+    //Code block is simply a multiline of statements
+    public static void main(String[] args) {
+
+        // if (x < y) { // begin a block by opening curly braces
+        //     x = y;
+        //     y = 0;
+
+        // } // end a block
+    }
+}
