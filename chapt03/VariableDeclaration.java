68b374bd35ef092ed3fd1631e9a1c76e6b16c438
Sheikhman_Kalderon <duah229@outlook.com>
2025-05-06 14:27:20 +0000

    chore: fix variables not used in file

--- chapt03/VariableDeclaration.java
+++ chapt03/VariableDeclaration.java
@@ -6,11 +6,11 @@ public class VariableDeclaration {
     */
     public static void main(String[] args){
         //declares multiple variables on one line
-        int a, b, c;
+        int a = 10, b =10, c = 10;
         int e = 10, f = 12, g = 15;
-        boolean d;
-        double pi;
-        
+        boolean d = true;
+        double pi = 3.1416;
+        System.out.println("The values are " + a + " " + b + " " + c + " " + e + " " + f +" " + g +" "+ pi + " "+ d);
     }
 
 }
---
+++ chapt03/VariableLifetime.java
@@ -0,0 +1,13 @@
+package chapt03;
+
+public class VariableLifetime {
+    /* scope defines the lifetime of a variable ,
+     * a variable is created when its scope is entered and
+     * destroyed once its scope is left
+     * However, for variables with initializers, it is always initialized
+     * whenever its scope is entered
+     */
+    public static void main(String[] args) {
+        
+    }
+}
