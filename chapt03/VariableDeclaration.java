40df17151568eb6402538a79081a38b5048dfedf
Sheikhman_Kalderon <duah229@outlook.com>
2025-05-06 13:06:43 +0000

    chore: add variable declaration refresher

---
+++ chapt03/VariableDeclaration.java
@@ -0,0 +1,15 @@
+package chapt03;
+
+public class VariableDeclaration {
+    /* Variable declaration demonstrated in java 
+     * follows TYPE IDENTIFIER (variable name) followed by equal then value
+    */
+    public static void main(String[] args){
+        //declares multiple variables on one line
+        int a, b, c;
+        int e = 10, f = 12, g = 15;
+        boolean d;
+        double pi;
+    }
+
+}
