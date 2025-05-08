81a9785804d1c4152c2aba0540a8d7ac778bf258
Sheikhman_Kalderon <duah229@outlook.com>
2025-05-08 08:46:31 +0000

    feature: arithmetic operations using integers and double

---
+++ chapt04/BasicMath.java
@@ -0,0 +1,34 @@
+package chapt04;
+
+public class BasicMath {
+    /*Basic Arithmetic Operators using */
+    public static void main(String[] args){
+        /* Integer Arithmetic */
+        int a =  1 + 1;
+        int b = a * 1;
+        int c = b / 4;
+        int d = c - 2;
+        int e = -d;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e = " + e);
+
+
+        //arithmetic operations using double
+        double da = 1 + 1;
+        double db = da * 1;
+        double dc = da / 4;
+        double dd = dc -2;
+        double de = -dd;
+        System.out.println("da = " + da);
+        System.out.println("db = " + db);
+        System.out.println("dc = " + dc);
+        System.out.println("dd = " + dd);
+        System.out.println("de = " + de);
+
+        
+    }
+}
