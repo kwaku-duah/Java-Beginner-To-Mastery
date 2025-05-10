01add5dd363290c6b35685d8e3cdf509c1bc1aef
Sheikhman_Kalderon <duah229@outlook.com>
2025-05-10 18:58:43 +0000

    feature: add for loops

---
+++ chapt05/ForLoop.java
@@ -0,0 +1,14 @@
+package chapt05;
+
+public class ForLoop {
+    /* demonstration of the for loop
+     * assumes the form for (initialization; condition; iteration) body or {body}
+     */
+    public static void main(String[] args) {
+        int n;
+
+        
+        for (n = 10; n > 0; n--)
+            System.out.println("tick " + n);
+    }
+}
