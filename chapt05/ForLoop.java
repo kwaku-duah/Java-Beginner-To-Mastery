ffa7296f198447b3525c47b84f14a9f1c72790b3
Sheikhman_Kalderon <duah229@outlook.com>
2025-05-10 19:00:24 +0000

    feature: add variant for the for loop

--- chapt05/ForLoop.java
+++ chapt05/ForLoop.java
@@ -7,7 +7,8 @@ public class ForLoop {
     public static void main(String[] args) {
         int n;
 
-        
+        // it is desirable to define the initialiser with type in the loop, if it wont be used elsewhere again
+        // would be for (int n = 10; n > 0; n--)
         for (n = 10; n > 0; n--)
             System.out.println("tick " + n);
     }
