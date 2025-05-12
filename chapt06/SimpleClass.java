ce2c259cc530af0936a96c09843218ec969c40ba
Sheikhman_Kalderon <duah229@outlook.com>
2025-05-12 20:55:47 +0000

    feature: add a simple structure for java class

---
+++ chapt06/SimpleClass.java
@@ -0,0 +1,12 @@
+package chapt06;
+
+public class SimpleClass {
+    /* this is a simple class
+     * keyword class followed by classname(identifier) {
+     * type instance-variable (what the class has e.g width and type methodname
+     * (parameters)) (what the class can do)}
+     */
+    double width;
+    double height;
+    double depth; //instance variables
+}
