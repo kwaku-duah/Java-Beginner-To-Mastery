1d393ba354b1ec31eee730cbfd62d905cac0268f
Sheikhman_Kalderon <duah229@outlook.com>
2025-05-12 05:00:30 +0000

    feature: using break to exit from nested loops with labels, civilized goto

--- chapt05/BreakNestedForLoops.java
+++ chapt05/BreakNestedForLoops.java
@@ -1,14 +1,15 @@
 package chapt05;
 
 public class BreakNestedForLoops {
-    /*if break is used in nested for loop,it only terminates that inner loop  */
+    /* if break is used in nested for loop,it only terminates that inner loop */
     public static void main(String[] args) {
         int i, j;
-        
-        for(i = 0; i < 3; i ++){
+
+        for (i = 0; i < 3; i++) {
             System.out.println("these are values for i: " + i);
             for (j = 0; j < 100; j++) {
-                if ( j == 10) break; //breaks only this loop
+                if (j == 10)
+                    break; // breaks only this loop
                 System.out.print("these are values of j: " + j);
                 System.out.println();
             }
