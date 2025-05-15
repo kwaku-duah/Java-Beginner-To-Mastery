87c5665c9a4177e515a4599afb571e3b2c0cb760
Sheikhman_Kalderon <duah229@outlook.com>
2025-05-15 20:52:03 +0000

    feature: add docs for the usage of this keyword

--- chapt06/ThisKeyword.java
+++ chapt06/ThisKeyword.java
@@ -5,16 +5,20 @@ public class ThisKeyword {
      * this helps an object reference the value of its current property 
      * and then take in a new value to replace it
      */
+    //all objects have these attributes
     double width;
     double height;
     double depth;
 
-    ThisKeyword(double w, double h, double d){
+    //temporary variables available only to this method
+    ThisKeyword(double width, double height, double depth){
         //using w, h, d in order to avoid variable shadowing
         //every object should have a reference to its value for an instance variable
 
-        this.width = w;
-        this. height = h;
-        this.depth = d;
+        //left hand side refers to the object invoking it with the instance variable
+        //right hand side is value of the temporary variable available to the method to replace the default value
+        this.width = width;
+        this. height = height;
+        this.depth = depth;
     }
 }
