efda9872a4594f005862d27b24af831848c31409
Sheikhman_Kalderon <duah229@outlook.com>
2025-05-20 15:33:09 +0000

    feature: add demonstration for static block use to initialize instance variables in java

--- chapt07/StaticBlock.java
+++ chapt07/StaticBlock.java
@@ -13,4 +13,31 @@ public class StaticBlock {
      * static methods cannot refer to any instance with "this" or "super"(used) in inheritance
      * because they refer to objects, whereas static cannot be used in any context of an object
      */
+    //initialized static variable
+    static int a = 3;
+
+    //uninitilized static variable
+    static int b;
+
+    static void metha(int x){
+        System.out.println("a " + a);
+        System.out.println("b " + b);
+        System.out.println("x " + x);
+    }
+
+    //opening up a static block to initialize b
+    static {
+        b = a * 4;
+    }
+
+     /*you don have to call the static method in terms of any object
+         * they are independent
+         * you can only call a static method within the confines of its class
+         * so ideally, I would have created another call to make this code 
+         * modular but it wont work
+         */
+        public static void main(String[] args) {
+            metha(42);
+        }
 }
+
