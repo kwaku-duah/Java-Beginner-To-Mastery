accd3f074f0218f731d85a669fd8d53c01dff853
Sheikhman_Kalderon <duah229@outlook.com>
2025-05-20 11:50:58 +0000

    docs: add definitions for access modifiers in java

---
+++ chapt07/AccessModifiers.java
@@ -0,0 +1,17 @@
+package chapt07;
+
+public class AccessModifiers {
+    /*access modifiers in java are basically public, private and
+    protected which is only used during inheritance
+     * public means that the member(code or data) is accessible to all codes
+     * in the program(that same class, other classes, and even other packages
+     * through importation)
+     * private means the members are only accessible to other members of that 
+     * same class
+     * NB: there is a scenario where no access modifier is used, java defaults
+     * to making those members public WITHIN ITS PACKAGE
+     * package refers to a group of classes
+     */
+
+     
+}
