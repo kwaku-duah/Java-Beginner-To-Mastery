ff31d805f224221f050139ad1b73bb06b1d454f4
Sheikhman_Kalderon <duah229@outlook.com>
2025-05-25 17:25:01 +0000

    feature: add demonstration for the use of the super keyword

--- chapt08/KeyWordSuper.java
+++ chapt08/KeyWordSuper.java
@@ -10,5 +10,49 @@ public class KeyWordSuper {
      * of the superclass and then secondly it can be used to access a member 
      * of a superclass which has been hidden by a member of a subclass
      */
-    
+
+     double width, height, depth;
+
+     //constructor when full dimensions are given
+     KeyWordSuper(double w, double h, double d){
+        width = w;
+        height = h;
+        depth = d;
+     }
+
+      //constructor to initialise an empty or  unuseable box
+    KeyWordSuper(){
+        width = -1;
+        height = -1;
+        depth = -1;
+    }
+
+    //constructor to initialise a cube
+    KeyWordSuper(double len) {
+        width = height = depth = len;
+    }
+
+    //constructor for an object to clone itself
+    KeyWordSuper(KeyWordSuper obj){
+        width = obj.width;
+        depth = obj.depth;
+        height = obj.height;
+    }
+
+}
+
+class BoxWeight extends KeyWordSuper{
+    //adding its own unique variable
+    double weight;
+
+    BoxWeight(double w, double d, double h, double m){
+        /* it is expected  to initialise these variables like
+         * width = w; height = h , depth = d, weight = m
+         * but super shortens that
+         */
+        super(w, d, h);
+        
+        //initialise only the variable specific to this
+        weight = m;
+    }
 }
