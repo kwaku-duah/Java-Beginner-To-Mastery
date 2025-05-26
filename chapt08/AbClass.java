81ee0abfeae33c04c1d02334a35f877cce1c1e30
Sheikhman_Kalderon <duah229@outlook.com>
2025-05-26 21:04:33 +0000

    docs: add rules for making abstract classes

---
+++ chapt08/AbClass.java
@@ -0,0 +1,16 @@
+package chapt08;
+
+public class AbClass {
+    /*abstract keyword
+     * superclasses can define structure of generalized classes without full implementation
+     * in that vein, abstract keyword is used to require that subclasses fully implement these methods
+     * it is of the form (abstract type name/identifier(param-list);)
+     * it has no body, so the subclass must implement it
+     * a class with one or more abstract classes must be prefixed with abstract
+     * there can be no object created from an abstract class and also there 
+     * cannot be an abstract constructor or abstric static methods
+     * a subclass created from a superclass must implement fully all abstract methods or be
+     * declared an abstract class itself
+     */
+    
+}
