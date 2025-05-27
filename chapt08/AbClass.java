a764fd5b72873b0f6f3d249366eb5a813ead7443
Sheikhman_Kalderon <duah229@outlook.com>
2025-05-27 10:54:00 +0000

    feature: add demonstration for abstract class in java

--- chapt08/AbClass.java
+++ chapt08/AbClass.java
@@ -1,6 +1,6 @@
 package chapt08;
 
-public class AbClass {
+abstract public class AbClass {
     /*abstract keyword
      * superclasses can define structure of generalized classes without full implementation
      * in that vein, abstract keyword is used to require that subclasses fully implement these methods
@@ -12,5 +12,28 @@ public class AbClass {
      * a subclass created from a superclass must implement fully all abstract methods or be
      * declared an abstract class itself
      */
-    
+
+     abstract void call();
+
+     //a concrete or normal method
+     void callmetoo(){
+        System.out.println("Normal method in java");
+     }
+}
+
+/*extending  */
+class B extends AbClass {
+    void call(){
+        System.out.println("Forced to implement");
+    }
+}
+
+class Practice {
+    //not possible for an abstract class to make objects
+    public static void main(String[] args) {
+        B obj = new B();
+
+        obj.call();
+        obj.callmetoo();
+    }
 }
---
+++ chapt08/RealFigure.java
@@ -0,0 +1,66 @@
+package chapt08;
+
+abstract public class RealFigure {
+    /* classic use case of an abstract class*/
+    double dim1, dim2;
+
+    RealFigure(double a, double b){
+        dim1 = a;
+        dim2 = b;
+    }
+
+    //method we cannot fully define here but need all extending classes to 
+    abstract double area();
+
+}
+
+class FigRect extends RealFigure{
+    
+    FigRect(double a, double b){
+        super(a,b);
+    }
+
+    double area() {
+        System.out.println("area of rectangle implemented to satisfy superclass condition");
+        return dim1 * dim2;
+    }
+}
+
+class FigTriangle extends FigRect{
+
+    FigTriangle(double a, double b){
+        super(a, b);
+    }
+
+    //triangle of a figure
+    double area() {
+        System.out.println("area of a triangle");
+        return dim1 * dim2 / 2;
+    }
+}
+
+class ShowExample{
+    public static void main(String[] args) {
+        //creating objects from the class
+        /*not allowed to create objects from abstract classes */
+        // RealFigure ref = new RealFigure(); this is illegal
+        FigRect obj = new FigRect(12.4, 23.5);
+
+        FigTriangle obj1 = new FigTriangle(67.5, 9.99);
+
+        /*however even for abstract classes, the superclass can be used as a reference for objects
+         * for inheritance and runtime polymorphism
+         */
+
+         RealFigure figref;
+
+         figref = obj;
+         System.out.println("Area for rectangle " + figref.area());
+
+         figref = obj1;
+         System.out.println("Area of triangle " + figref.area());
+
+
+
+    }
+}
