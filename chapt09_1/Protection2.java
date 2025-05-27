f235a2c74838de0dff4b8759bafb3637a0b40fe5
Sheikhman_Kalderon <duah229@outlook.com>
2025-05-27 18:54:54 +0000

    feature: demonstrate access of fields in other packages

--- chapt09_1/Protection2.java
+++ chapt09_1/Protection2.java
@@ -12,8 +12,12 @@ public class Protection2 extends chapt09.Protection {
         //accessing private field from another package
         // System.out.println("private field in another package" + n_pri);
 
-        //accessing the public field in the other package
-        System.out.println(n_pro); //this field is accessible because it is public
+        //accessing the protected field in the other package
+        System.out.println(n_pro); //this field is accessible because it is protected
+
+        //accessing public field
+        System.out.println(n_pub);
+
 
     }
 
