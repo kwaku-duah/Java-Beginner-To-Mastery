7f084b53f5a4ac71349a4abfd8a8b7e42ffef7da
Sheikhman_Kalderon <duah229@outlook.com>
2025-05-27 19:32:11 +0000

    feature: structure of import statements usage

---
+++ chapt09/ImportStatement.java
@@ -0,0 +1,14 @@
+package chapt09;
+
+public class ImportStatement extends java.util.Date {
+    /*in java, java.lang.* is implicitly imported by the compiler
+     * every import starts with (top level package name).(subordinate package name)
+     * .specific class name of the form
+     * java.util.Date or to import a whole java.io.* 
+     * 
+     * public class ImportStatement extends java.util.Date this is valid same 
+     * as public class ImportStatement extends Date; after 
+     * package name/identifier;
+     * import java.util.*;
+     */
+}
