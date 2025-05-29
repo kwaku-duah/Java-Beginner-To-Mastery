9ef37f9a73d562974d4e00c942b29900b07e7854
Sheikhman_Kalderon <duah229@outlook.com>
2025-05-29 16:10:07 +0000

    feature: add  implementation of an interface without default method

---
+++ chapt09/ImplDefault.java
@@ -0,0 +1,10 @@
+package chapt09;
+
+public class ImplDefault implements DefaultInterface {
+    //it has two methods, normal and default, only normal method is required
+
+    public int normalMethod(){
+        return 100;
+    }
+    
+}
