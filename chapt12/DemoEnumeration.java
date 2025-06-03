ff10262e1fc1ca0057819f583956e3d6687602b5
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-03 09:52:26 +0000

    feature: add way of instantiating enum and using relational operators

--- chapt12/DemoEnumeration.java
+++ chapt12/DemoEnumeration.java
@@ -19,4 +19,18 @@ public class DemoEnumeration {
       they are implicity final, static and final and of the type of the enclosing 
       enumeration in which they are declared, here of type Apple, they are self-type
       */
+
+      /*
+       * after creating an enumeration, a reference variable can be created to reference it
+       * 
+       */
+      Apple ap; //ap is a reference of type Apple
+
+      //ap can hold values of only values defined in apple
+     ap = DemoEnumeration.Apple.Golddel;
+
+     //enumeration constants can also be compared with the relational operator
+     if(ap == Apple.Golddel);
+
+     
 }
