b67007d044a37485a7c1acb4a070461869434582
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-03 14:34:46 +0000

    docs: add brief documentation regarding type wrappers in java

---
+++ chapt12/TypeWrapper.java
@@ -0,0 +1,21 @@
+package chapt12;
+
+
+public class TypeWrapper {
+    /*Type wrappers are very important
+     * primitive(simple) data types made way for efficiency, performance-wise
+     * however, they are not objects and do not inherit from objects
+     * However, sometimes you may want to pass it a type reference, primitive
+     * data types cannot, hence wrapping them to become objects become necessary
+     * it is done with a static method value0f
+     * the wrapper classes are Byte, Long, Short, Int, Float, Double, Character
+     * and Boolean
+     * to make them objects
+     * static (wrapperClassType) valueOf(primitive data type ref-for-object-to-wrap)
+     * to get the value from a wrapper class 
+     * (primitive type) primitiveType+Value() e.g char charValue(), boolean booleanValue()
+     * byte byteValue()
+     */
+    
+     //demonstration of type wrapping
+}
