db4d9cf92a8bec13c713f619c9d478030515e033
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-03 10:23:23 +0000

    feature: add switch statement using enums

--- chapt12/EnumDemo.java
+++ chapt12/EnumDemo.java
@@ -18,5 +18,25 @@ public class EnumDemo {
         //compare two enum values
         if (ap == Apple.Golddel)
             System.out.println("ap contains Goldendel.\n");
+            
+
+        //use enum to control a switch statement
+        switch (ap) {
+            case Jonathan:
+                System.out.println("Jonathan is red");
+                break;
+            case Reddel:
+                System.out.println("Reddel is yellow");
+                break;
+            case Golddel:
+                System.out.println("Golddel delicious is red");
+                break;
+            case Winesap:
+                System.out.println("Winesap is red");
+                break;
+            case Cortland:
+                System.out.println("Cortland is red");
+                break;
+        }
     }
 }
