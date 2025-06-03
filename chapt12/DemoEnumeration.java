b252de66d244d562bc8a0d5af4e4e359f59a2f3d
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-03 09:40:48 +0000

    feature: add structure for creating an enumeration in java

---
+++ chapt12/DemoEnumeration.java
@@ -0,0 +1,22 @@
+package chapt12;
+
+public class DemoEnumeration {
+    /*enumeration is very important in the context of java
+     * it refers to a list of named constants and its legal values
+     * it define a class type
+     * enumerations can have constructors, methods and instance variables, 
+     * making it extremely powerful
+     * enumeration is created using the enum keyword
+     */
+
+     //demo
+     enum Apple {
+        Jonathan, Golddel, Reddel
+     }
+
+     /* Jonathan,Golddel are enumeration constants
+      * enumeration is a special kind of class
+      they are implicity final, static and final and of the type of the enclosing 
+      enumeration in which they are declared, here of type Apple, they are self-type
+      */
+}
