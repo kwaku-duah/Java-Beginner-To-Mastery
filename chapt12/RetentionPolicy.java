7421d47a39a7c06fcef9c57102dcf1949e135e1a
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-04 14:08:09 +0000

    docs: add docs about retention policy in java

---
+++ chapt12/RetentionPolicy.java
@@ -0,0 +1,11 @@
+package chapt12;
+
+public class RetentionPolicy {
+    /*Retention policy in java indicates when an annotation should be discarded
+     * it has three values in an enum in java.lang.annotation.RetentionPolicy
+     * SOURCE - only retained in the source and discarded
+     * CLASS - only remains in the .class file and not available to the JVM
+     * RUNTIME - remains in the .class file and available to JVM  - thus runtime
+     * policy offers the greatest annotation persistence in java
+     */
+}
