0cea321c0f7beec59107f22ab35dd96a40936207
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-04 14:12:19 +0000

    feature: add usage of retention policy in java for annotation persistence

--- chapt12/RetentionPolicy.java
+++ chapt12/RetentionPolicy.java
@@ -1,5 +1,7 @@
 package chapt12;
 
+import java.lang.annotation.Retention;
+
 public class RetentionPolicy {
     /*Retention policy in java indicates when an annotation should be discarded
      * it has three values in an enum in java.lang.annotation.RetentionPolicy
@@ -7,5 +9,14 @@ public class RetentionPolicy {
      * CLASS - only remains in the .class file and not available to the JVM
      * RUNTIME - remains in the .class file and available to JVM  - thus runtime
      * policy offers the greatest annotation persistence in java
+     * if no retention policy is specified, it defaults to CLASS
+     * precedes annotation creation
      */
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface MyAnnot {
+        String str();
+        int val();
+    }
+
+
 }
