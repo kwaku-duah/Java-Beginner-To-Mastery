c7e66f1fb446bde3c46899ed8db44d4c8b1aff33
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-04 13:56:31 +0000

    feature: add docs on annotation and making an annotation type

---
+++ chapt12/Annotation.java
@@ -0,0 +1,20 @@
+package chapt12;
+
+public class Annotation {
+    /*annotations are supplemental information embedded in source files
+     * does not change funtionality or semantics, just necessary information 
+     * necessary for deployment and deployment
+     * 
+     * all annotation types start with @, and they contain solely method 
+     * declarations without bodies, Java implements it
+     * 
+     * annotation types cannot use the extends keyword, they all are subclasses 
+     * of Annotation class which has methods etc
+     */
+    @interface MyAnno {
+        String str();
+        int val();
+    }
+
+ 
+}
