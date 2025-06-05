e686e384e471da3f54453269b009d35f5a221313
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-05 11:16:05 +0000

    feature: add demonstration of consumption of an annotation with a parameter

--- chapt12/DemonstrateTypeAnnotations.java
+++ chapt12/DemonstrateTypeAnnotations.java
@@ -7,48 +7,45 @@ import java.lang.annotation.Target;
  * 
  */
 
- //a marker annotation that can be applied to a type
+//a marker annotation that can be applied to a type
 @Target(ElementType.TYPE_USE)
-@interface TypeAnno {}
+@interface TypeAnno {
+}
 
-
-//annotation that can be applied to a type
+// annotation that can be applied to a type
 @Target(ElementType.TYPE_USE)
-@interface NotZeroLen {}
+@interface NotZeroLen {
+}
 
-//still annotation that can be applied to a type
+// still annotation that can be applied to a type
 @Target(ElementType.TYPE_USE)
-@interface unique{}
+@interface unique {
+}
 
-
-//parameterized annotation that can be applied to a type
+// parameterized annotation that can be applied to a type
 @Target(ElementType.TYPE_USE)
 @interface MaxLex {
     int value();
 }
 
-
-//annotation that can be applied to a type parameter
+// annotation that can be applied to a type parameter
 @Target(ElementType.TYPE_PARAMETER)
 @interface What {
     String description();
 }
 
-//annotation that can be applied to a field declaration
+// annotation that can be applied to a field declaration
 @Target(ElementType.FIELD)
-@interface EmptyOK{ }
+@interface EmptyOK {
+}
 
-
-//annotation that can be applied to a method declaration
+// annotation that can be applied to a method declaration
 @Target(ElementType.METHOD)
-@interface Recommended {}
+@interface Recommended {
+}
 
+// using annotation on a type parameter (we defined a parameterized annotation
+// called WHAT)
+public class DemonstrateTypeAnnotations<@What(description = "Generic type description") T> {
 
-
-
-
-
-
-public class DemonstrateTypeAnnotations {
-    
 }
