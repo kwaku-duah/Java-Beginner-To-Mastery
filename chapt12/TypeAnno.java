afbc2c88fd54bb8906206efb7807763c543bec80
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-05 10:49:57 +0000

    feature: add demonstration of using a custom type annotation to annotate (this) which is of type of its class in java

--- chapt12/TypeAnno.java
+++ chapt12/TypeAnno.java
@@ -26,9 +26,18 @@ public class TypeAnno {
     /*annotation for this is allowed in java in modern editions, (this) is an argument which is implicit
     for all instances,it can be declared as first argument, in the declaration, the type of (THIS) should be its class
      */
-    
+
     void myMethanol(TypeAnno this, int i){
         //here this is of type TypeAnno CLASS
+    }
+
+    //assuming TypeAnnot is a valid annotation, it is being for this, 
+    /*(this) should always be the first parameter if you want to annotate, 
+     * because it is implicit, it will either way be there already, list it if you want to categorically 
+     * annotate it
+     */
+    int myButanoal(@TypeAnnot TypeAnno this, int j) {
+        return 0;
     }
 
 }
