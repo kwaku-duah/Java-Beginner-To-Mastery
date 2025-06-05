855c3927543a29f925946d2e42e41d97d43e87f6
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-05 11:11:40 +0000

    feature: add implementation of several type annotations

---
+++ chapt12/DemonstrateTypeAnnotations.java
@@ -0,0 +1,54 @@
+package chapt12;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/*marker annotations that can be applied to a type
+ * 
+ */
+
+ //a marker annotation that can be applied to a type
+@Target(ElementType.TYPE_USE)
+@interface TypeAnno {}
+
+
+//annotation that can be applied to a type
+@Target(ElementType.TYPE_USE)
+@interface NotZeroLen {}
+
+//still annotation that can be applied to a type
+@Target(ElementType.TYPE_USE)
+@interface unique{}
+
+
+//parameterized annotation that can be applied to a type
+@Target(ElementType.TYPE_USE)
+@interface MaxLex {
+    int value();
+}
+
+
+//annotation that can be applied to a type parameter
+@Target(ElementType.TYPE_PARAMETER)
+@interface What {
+    String description();
+}
+
+//annotation that can be applied to a field declaration
+@Target(ElementType.FIELD)
+@interface EmptyOK{ }
+
+
+//annotation that can be applied to a method declaration
+@Target(ElementType.METHOD)
+@interface Recommended {}
+
+
+
+
+
+
+
+public class DemonstrateTypeAnnotations {
+    
+}
