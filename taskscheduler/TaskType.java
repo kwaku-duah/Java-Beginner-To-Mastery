7501acbc685ea95db3ba701a967657c1bf590f2d
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-06 09:11:13 +0000

    feature: add tasktypes using enumeration

---
+++ taskscheduler/TaskType.java
@@ -0,0 +1,9 @@
+package taskscheduler;
+
+public enum TaskType {
+    /*enumeration - list of named constants
+     * a type of TaskType can only use valid values or members 
+     */
+
+     DOWNLOAD, BACKUP, SYNC
+}
