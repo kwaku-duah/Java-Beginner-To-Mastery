20468e634fce18e7e03cbc4309d27564949feb42
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-06 11:21:45 +0000

    feature: add taskrunner to control threads

--- taskscheduler/TaskRunner.java
+++ taskscheduler/TaskRunner.java
@@ -1,15 +1,17 @@
 package taskscheduler;
 
 public class TaskRunner {
-    /*implementing threading for the tasks and name of tasks */
-    public static void runTask(Task t, int delayInMilli){
+    /* Implements threading with delay before executing the task */
+    public static void runTask(Task t, int delayInMilli) {
 
         try {
             Thread.sleep(delayInMilli);
             t.run();
-            
         } catch (InterruptedException e) {
-            System.out.println("Method handled " + e);
+            System.out.println("Interrupted "  + e);
         }
+        Thread threadDefined = new Thread(t); //constructor in Runnable that a Runnable task
+        threadDefined.start(); 
     }
+
 }
