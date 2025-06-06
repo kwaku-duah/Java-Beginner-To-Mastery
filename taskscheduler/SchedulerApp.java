91d0e4a832a8bbc6f55dd201dbda9c16b17b8cee
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-06 13:28:12 +0000

    chore: add text corrections

--- taskscheduler/SchedulerApp.java
+++ taskscheduler/SchedulerApp.java
@@ -3,12 +3,12 @@ package taskscheduler;
 public class SchedulerApp {
     public static void main(String[] args) {
         Task task1  = new Task("BackUpDocuments", TaskType.BACKUP);
-        Task task2 = new Task("DownloadDocume", TaskType.DOWNLOAD);
+        Task task2 = new Task("DownloadDocuments", TaskType.DOWNLOAD);
         Task task3 = new Task("SYNC TASKS",TaskType.SYNC);
 
         TaskRunner.runTask(task1, 1000);
         TaskRunner.runTask(task2, 3000);
-        TaskRunner.runTask(task3, 5000);
+        TaskRunner.runTask(task3, 000);
 
         System.out.println("Scheduled tasks..");
     }
