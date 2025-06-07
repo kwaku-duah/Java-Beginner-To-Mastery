727a979d2353a6bcfaed454af52dc5efe0201522
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-07 23:41:36 +0000

    docs: add brief docs about assert keyword and usage in java

---
+++ chapt13/AssertDemo.java
@@ -0,0 +1,15 @@
+package chapt13;
+
+public class AssertDemo {
+    /*assert keyword is used to demonstrate an assertion
+     * it takes the form 
+     * assert condition, where condition evals to boolean
+     * if it evals to true, nothing is done, otherwise, an AssertError is thrown
+     * 
+     * it also has another format
+     * assert condition: expres;
+     * where expression takes a value, non-void value that will be thrown 
+     * when it fails
+     */
+    
+}
