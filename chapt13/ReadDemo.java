29320ab8cfe05fa37f0aad544046f42d6f6223b6
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-07 15:10:12 +0000

    feature: add reading a file with read from fileinputstream

---
+++ chapt13/ReadDemo.java
@@ -0,0 +1,41 @@
+package chapt13;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ReadDemo {
+    /*used read method to read from a file with fileinputsteam
+     * this file showcases the use of manually calling close() method
+     * 
+     */
+    public static void main(String[] args) {
+        int i;
+        FileInputStream fin;
+
+        //first confirm an argument is specified the commandline
+        if(args.length != 1) 
+        {System.out.println("No file has been specified");
+        return;
+}
+        //attempt to open the file
+        try {
+            fin = new FileInputStream(args[0]);
+        } catch (FileNotFoundException e) {
+            System.out.println("No such file exists");
+            return;
+        }
+
+        //if code reaches here it means the file is open and can be read
+        try {
+            do {
+                i = fin.read() ;
+                if (i != -1) System.out.println((char) i);
+            } while (i != 1);
+        } catch (IOException e) {
+            System.out.println("File cannot be opened");
+        }
+
+    }
+}
