fc63abd11e85a3da157c094ac60877db5d211389
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-07 14:36:27 +0000

    docs: add brief docs on PrintWriter class

---
+++ chapt13/PrintWriter.java
@@ -0,0 +1,13 @@
+package chapt13;
+
+public class PrintWriter {
+    /*character based approach for outputting to the console
+     * modern way, takes the form 
+     * PrintWriter(OutputStream var, boolean flushingOn)
+     * PrintWrite is a class and flushingON being TRUE
+     * means - output immediately and false being otherwise
+     * Although system.out is an accepted way of writing to the console,
+     * PrintWriter is the accepted of doing such, it is internationalize
+     */
+    
+}
