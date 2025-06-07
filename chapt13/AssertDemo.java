605e94d275b3b542c2c1684dd60954088091bd30
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-07 23:53:19 +0000

    feature: assert could also take another form where asserterror throws your custom message

--- chapt13/AssertDemo.java
+++ chapt13/AssertDemo.java
@@ -11,5 +11,22 @@ public class AssertDemo {
      * where expression takes a value, non-void value that will be thrown 
      * when it fails
      */
-    
+    static int val = 3;
+
+    static int getNum() {
+        return val--;
+    }
+
+    public static void main(String[] args) {
+        int n;
+
+        for (int i = 0; i < 10; i++) {
+            n = getNum();
+
+            //demonstrate use of assert keyword
+            assert n > 0: "assert is not positive";
+            System.out.println("n is " + n);
+        }
+    }
+
 }
