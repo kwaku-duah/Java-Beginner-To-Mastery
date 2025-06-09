09e2d2fe00529ddfe4e18d20cd35ab2922a6adc6
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-09 14:26:36 +0000

    feature: add demonstration for use of bounded wildcards which gives flexibility

--- chapt14/BoundedWildCards.java
+++ chapt14/BoundedWildCards.java
@@ -28,18 +28,77 @@ class FourD extends ThreeD {
 
 }
 
-//next here is a generic class that specifies TwoD as an upper boundary
+// next here is a generic class that specifies TwoD as an upper boundary
 class Coords<T extends TwoD> {
     T[] coords;
 
     Coords(T[] o) {
         coords = o;
     }
+
 }
+
+// a method that shows what is in the arrays, at least we know that it has at
+// least an x and y coordinate
 
 public class BoundedWildCards {
     /*
      * wildcards are represented with ? indicating unknown types
      * wildcards can be bounded pretty much the same way as type parameters
      */
+    // method to show objects in Coords
+    static void showXY(Coords<?> c) {
+        System.out.println("X and Y coordinates");
+        for (int i = 0; i < c.coords.length; i++) 
+            System.out.println("x and y component of the object " + c.coords[i].x + ", " + c.coords[i].y);
+            
+            System.out.println();
+    }
+
+    static void showXYZ(Coords<? extends ThreeD> c) {
+        System.out.println("X, Y, Z coordinates");
+        for (int i = 0; i < c.coords.length; i++) System.out.println("x, y and z components " +  c.coords[i].x + " " +  c.coords[i].y + " " +  c.coords[i].y);
+
+        System.out.println();
+        
+    }
+
+    static void showXYZT(Coords<? extends FourD> c) {
+        for (int i = 0; i < c.coords.length; i++) System.out.println("X, Y, Z, T coordinates " + 
+        c.coords[i].x + " " + c.coords[i].y + " " +  c.coords[i].z + " " +  c.coords[i].t);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        TwoD[] one  = {
+            new TwoD(0, 0),
+            new TwoD(9, 7),
+            new TwoD(18, 4),
+            new TwoD(-1, -23)
+        };
+
+        //two D
+        Coords<TwoD> tdlocs = new Coords<TwoD>(one);
+        System.out.println("Contents of tdlocs");
+
+        showXY(tdlocs);
+        // showXYZ(tdlocs);
+        // showXYZT(tdlocs);
+
+       // three D 
+        ThreeD[] thdlocs = {
+            new ThreeD(1, 3, 5),
+            new ThreeD(3, 6, 7),
+            new ThreeD(5, 7, 5)
+        };
+        System.out.println("Contents of three dimensional ");
+        Coords<ThreeD> th = new Coords<ThreeD>(thdlocs);
+
+        showXYZ(th);
+        
+    }
+
+    
+
+    
 }
