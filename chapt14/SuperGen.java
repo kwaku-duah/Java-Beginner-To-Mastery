c1f6281a93d34437e1d0b360e2fc5aa32981cc39
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-09 21:28:05 +0000

    feature: add implementation of generic subclass having its own instance variables after inheriting from a superclass

--- chapt14/SuperGen.java
+++ chapt14/SuperGen.java
@@ -12,13 +12,30 @@ class Gen<T> {
 }
 
 //subclass
-class Gen2<T> extends Gen<T> {
+/* whether or not a subclass would use the type parameter, it must pass
+ * the T parameters in its super class, it can add its own variables to it too
+ */
+class Gen2<T, V> extends Gen<T> {
 
-    Gen2(T o) {
+    V ob2;
+
+    Gen2(T o, V o2) {
         super(o);
+        ob2 = o2;
+    }
+
+    //get ob2
+    V getOb2() {
+        return ob2;
     }
 }
 
 public class SuperGen {
-    
+    public static void main(String[] args) {
+        //creating an object of type gen 2
+        Gen2<Integer, String> ob = new Gen2<Integer, String>(88, "Generics Test");
+
+        System.out.println("value is " + ob.getOb());
+        System.out.println("value is " + ob.getOb2());
+    }
 }
