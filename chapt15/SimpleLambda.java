368dcd8e2cbd697e4dddbbaeb4c78294c31ed581
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-10 20:33:43 +0000

    feature: add a lambda expression that takes a parameter

--- chapt15/SimpleLambda.java
+++ chapt15/SimpleLambda.java
@@ -1,24 +1,30 @@
 package chapt15;
 
 public class SimpleLambda {
-//writing a lambda class
-//LHS hold parameter list
-//RHS holds expression body
+    // writing a lambda class
+    // LHS hold parameter list
+    // RHS holds expression body
 
-//simplest demonstration of a lambda expression
-SimpleLambda() {
+    // simplest demonstration of a lambda expression
+    SimpleLambda() {
 
-}
+    }
 
-// () -> 123.4; 
-//takes no parameters and it returns 123.4
+    // () -> 123.4;
+    // takes no parameters and it returns 123.4
 
-//this expression is similar to 
-    double myMeth() {return 123.4;}
+    // this expression is similar to
+    double myMeth() {
+        return 123.4;
+    }
 
-public static void main(String[] args) {
-    
-    
-}
+    // () -> Math.random() * 100; //another example of a lambda expression
+
+    //a lambda method that takes a paramter list
+    //(n) -> (n % 2) == 0;
+
+    public static void main(String[] args) {
+
+    }
 
 }
