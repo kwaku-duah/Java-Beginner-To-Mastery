2438e6bf4b36deb424ce13a089c8cd816e9698a4
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-10 22:06:46 +0000

    feature: add demonstration of single use of lambda expressions

--- chapt15/Demo.java
+++ chapt15/Demo.java
@@ -17,5 +17,12 @@ public class Demo {
         //lambda expression implements the test method in the interface
         System.out.println("Is 2 modulo 2 an even number? " + lam.test(2));
 
+        //check non-even
+        if(lam.test(20)) System.out.println("It is not, this should print");
+
+        //use lambda expression to test if a number is non negative
+        LambdaParameter pam = (n) -> (n) >0;
+        if(pam.test(10)) System.out.println("Non-negative");
+
     }
 }
