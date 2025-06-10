8a08cd397d5744169895a98c49189d45399ebfb3
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-10 20:42:57 +0000

    feature: add a functional interface syntax demonstration

---
+++ chapt15/FuncInterface.java
@@ -0,0 +1,9 @@
+package chapt15;
+
+
+interface FuncInterface {
+    //functional interface contains only one abstract method
+    double getValue();
+
+    //this is implicitly abstract and the function is getValue();
+}
