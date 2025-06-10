adf4dabebfb50a9b50885d11fa827192d1ef7819
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-10 21:24:54 +0000

    feature: add implementation of a functional interface by a lambda expression

---
+++ chapt15/AssignLamb.java
@@ -0,0 +1,29 @@
+package chapt15;
+
+
+interface FuncInterface {
+
+    //functional interface contains only one abstract method (implicitly abstract)
+    abstract double getValue();
+
+    //this is implicitly abstract and the function is getValue();
+}
+
+public class AssignLamb {
+    //reference to instance variable for the interface
+    FuncInterface refFun;
+
+    public AssignLamb(){
+         refFun = () -> 123.4;
+    }
+
+    public static void main(String[] args) {
+        AssignLamb ob = new AssignLamb();
+        System.out.println(ob.refFun.getValue());
+    }
+}
+
+
+
+
+
--- chapt15/FuncInterface.java
+++
@@ -1,9 +0,0 @@
-package chapt15;
-
-
-interface FuncInterface {
-    //functional interface contains only one abstract method
-    double getValue();
-
-    //this is implicitly abstract and the function is getValue();
-}
