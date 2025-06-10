58059d495db7b7fa3358442dc4036acbbbed8902
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-10 20:25:35 +0000

    feature: add syntax of a lambda expression|

---
+++ chapt15/SimpleLambda.java
@@ -0,0 +1,17 @@
+package chapt15;
+
+public class SimpleLambda {
+//writing a lambda class
+//LHS hold parameter list
+//RHS holds expression body
+
+//simplest demonstration of a lambda expression
+SimpleLambda() {
+
+}
+
+public static void main(String[] args) {
+    // () -> 123.4; takes no parameters and it returns 123.4
+}
+
+}
