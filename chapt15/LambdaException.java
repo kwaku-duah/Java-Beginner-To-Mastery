b49f4c01b227499ac28772d878f4685dcea8825e
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-11 16:50:02 +0000

    feature: add lambda expression using exception defined by functional interface during implementation

---
+++ chapt15/LambdaException.java
@@ -0,0 +1,35 @@
+package chapt15;
+
+
+interface FunctInterface {
+    double f(double[] n) throws EmptyArrayException;
+}
+
+class EmptyArrayException extends Exception {
+    public EmptyArrayException() {
+        super("Array is empty");
+    }
+}
+
+
+public class LambdaException {
+    //lambda expressions may throw exceptions thrown by the functional interface interface
+    public static void main(String[] args) throws EmptyArrayException {
+        double[] values = { 1.0, 2.0, 3.0, 4.0, 5.0};
+
+        FunctInterface average = (n) -> {
+            double sum = 0;
+            //check if anything exists
+            if(n.length == 0)
+                throw new EmptyArrayException();
+
+            for (int i = 0; i < n.length; i++)
+                sum += n[i];
+
+            return sum / n.length;
+        };
+
+        System.out.println("The average is " + average.f(values));
+
+    }
+}
