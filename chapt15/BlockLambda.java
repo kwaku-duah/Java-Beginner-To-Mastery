d9b2f683a394d86d61bf45c0a6ee539f21a69e7d
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-11 11:27:55 +0000

    feature: add block lamba demonstration example

--- chapt15/BlockLambda.java
+++ chapt15/BlockLambda.java
@@ -1,5 +1,23 @@
 package chapt15;
 
+/*lambda that computes the factorial of an int */
+interface NumericFun {
+    int func(int n);
+}
+
 public class BlockLambda {
-    //expression of lambda have a block body
+    // expression of lambda have a block body
+    public static void main(String[] args) {
+        // reference to a functional interface
+        NumericFun factorial = (n) -> {
+            int result = 1;
+            for (int i = 1; i <= n; i++)
+                result = i * result;
+
+            return result;
+
+        };
+        System.out.println("Factorial of three " + factorial.func(3));
+        System.out.println("factorial of 50 " + factorial.func(5));
+    }
 }
