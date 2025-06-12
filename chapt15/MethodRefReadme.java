bc9950257de3705896978ecdb365e249859a5946
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-12 09:56:11 +0000

    feature: add methods with functional interface as a parameter

--- chapt15/MethodRefReadme.java
+++ chapt15/MethodRefReadme.java
@@ -7,10 +7,35 @@ package chapt15;
  * 
  * 
  * General syntax for  static method reference
- * ClassName::methodName
+ * ClassName::methodName, :: a separator
+ * this also needs a functional interface
  */
 
 
+ interface StringFunc{
+    String func(String n);
+ }
+
+//this class defines a static method
 public class MethodRefReadme {
-    
+    static String strReverse(String str) {
+
+        String reverse = "";
+        int i;
+        for (i = str.length()-1; i >=0; i--)
+            reverse += str.charAt(i);
+        return reverse;
+    }
+}
+
+class DemoMethodRef {
+    /*
+     * this method has a functional interface as its first parameter
+     * hence it can take instance of a functional interface including 
+     * a method reference
+     */
+    static String stringOp(StringFunc sf, String s) {
+        return sf.func(s);
+    }
+
 }
