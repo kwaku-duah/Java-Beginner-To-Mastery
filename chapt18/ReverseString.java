ab2d637e0a38a0adcfdfd063b63b9a45eacc2ab9
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-13 20:51:33 +0000

    feature: add stringbuffer reverse method

---
+++ chapt18/ReverseString.java
@@ -0,0 +1,11 @@
+package chapt18;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        //using reverse from stringbuffer class
+        StringBuffer s = new StringBuffer("KWAKU DUAH");
+
+      StringBuffer a = s.reverse();
+      System.out.println(a);
+    }
+}
