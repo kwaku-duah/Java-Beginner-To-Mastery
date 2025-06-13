1b59fabbe33a2f824ba7d27be63818de2ad5cc99
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-13 15:38:07 +0000

    feature: add demonstration for a text block using the least spaced line as default for clearing whitespaces

---
+++ chapt17/DemonsBlock.java
@@ -0,0 +1,14 @@
+package chapt17;
+
+public class DemonsBlock {
+    // demonstration of text block
+    public static void main(String[] args) {
+        String str = """
+                Text blocks support indentation and multiple
+                lines in code.
+                        It reduces the tedium of having to
+                            navigate the stress of multilines.
+                """;
+        System.out.println(str);
+    }
+}
