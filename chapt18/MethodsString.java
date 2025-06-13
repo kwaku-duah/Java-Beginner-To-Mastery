bc356a77eea60da4b1d81478661f3b977a388a50
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-13 20:26:52 +0000

    feature: add string method length()

---
+++ chapt18/MethodsString.java
@@ -0,0 +1,16 @@
+package chapt18;
+
+
+/*
+ * string method  - length
+ */
+
+public class MethodsString {
+    public static void main(String[] args) {
+        char[] ch = {'a', 'b' , 'c'};
+
+        String a = new String(ch);
+        System.out.println(a.length());
+
+    }
+}
