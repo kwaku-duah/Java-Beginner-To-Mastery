ce422bf3f07671cbb7396c3947af21f4a6d8fc5b
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-13 15:57:59 +0000

    docs: add brief docs on records and advantages

---
+++ chapt17/StructureRecord.java
@@ -0,0 +1,13 @@
+package chapt17;
+
+/* records are a new addition in java
+ * super useful as they already implement a getter and setter classes for fields
+ * records reduce verbose code, instantialise all fields and and make it private
+ * it uses canonical constructors and it is used to create aggregated data
+ * record is created with the keyword record
+ */
+
+
+public class StructureRecord {
+    
+}
