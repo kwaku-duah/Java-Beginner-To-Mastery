e37918b2a52211a6fce00f1dbbc2b786ce247ad8
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-13 13:16:19 +0000

    docs: add brief docs on what are text blocks

---
+++ chapt17/TextBlock.java
@@ -0,0 +1,12 @@
+package chapt17;
+
+/*new addition to java is text blocks
+ * of the form """ """" a sequence of characters that can occupy more
+ * than three lines
+ */
+
+public class TextBlock {
+    public static void main(String[] args) {
+        
+    }
+}
