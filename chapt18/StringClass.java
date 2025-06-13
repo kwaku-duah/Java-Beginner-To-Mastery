4b81574d9d712eebaec7facab182185a65ff10a4
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-13 20:22:00 +0000

    feature: add string class format

---
+++ chapt18/StringClass.java
@@ -0,0 +1,13 @@
+package chapt18;
+
+
+public class StringClass {
+    /* string is a class in java 
+     * and 
+    */
+    public static void main(String[] args) {
+        String word = new String();
+        
+       
+    }
+}
