9b1eb4e60ef8db82d5bb024da3b92e5c2ff676ad
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-14 13:34:49 +0000

    docs: add brief intro about linkedlist class

---
+++ chapt20/DemoLinkedlist.java
@@ -0,0 +1,10 @@
+package chapt20;
+
+public class DemoLinkedlist {
+    public static void main(String[] args) {
+        /*linked list implements the dequeue,list and queue interface 
+         * it has great methods like getFirst(), offerFirst() addLast() etc
+        */
+
+    }
+}
