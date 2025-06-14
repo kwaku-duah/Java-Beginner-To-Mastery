4040c657bd7c4675b2489808947c7759916de630
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-14 14:30:25 +0000

    feature: add implementation of treeset example

---
+++ chapt20/SDemoTreeTest.java
@@ -0,0 +1,23 @@
+package chapt20;
+
+import java.util.TreeSet;
+
+/*
+ * Tree set is great for sorting, 
+ * sorts data and is fast
+ */
+
+public class SDemoTreeTest {
+   public static void main(String[] args) {
+     TreeSet<String> hold = new TreeSet<String>();
+
+    hold.add("F");
+    hold.add("A");
+    hold.add("5");
+    hold.add("a");
+
+    System.out.println("Display the contents " + hold);
+   }
+
+    
+}
