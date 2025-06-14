64445c61843ad0e12afe2d85b289d813cd32e0df
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-14 13:27:11 +0000

    feature: add arraylist in collections  usage of method add

---
+++ chapt20/DemoArrayList.java
@@ -0,0 +1,22 @@
+package chapt20;
+
+import java.util.*;
+
+public class DemoArrayList {
+    public static void main(String[] args) {
+        //ArrayList is a generic class in Java
+        ArrayList<String> test = new ArrayList<String>();
+
+        //additions to the array list
+        test.add("a");
+        test.add("5");
+
+        //display all
+        System.out.println("Show all members in the arraylist " + test);
+
+
+    
+
+
+    }
+}
