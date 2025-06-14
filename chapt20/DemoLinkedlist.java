cc2dce0cc190ec44edebc91a9fab6aa301aca790
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-14 13:40:08 +0000

    feature: add implementation of linked list

--- chapt20/DemoLinkedlist.java
+++ chapt20/DemoLinkedlist.java
@@ -1,10 +1,23 @@
 package chapt20;
 
+import java.util.LinkedList;
+
 public class DemoLinkedlist {
     public static void main(String[] args) {
         /*linked list implements the dequeue,list and queue interface 
          * it has great methods like getFirst(), offerFirst() addLast() etc
         */
+        LinkedList<String> test = new LinkedList<>();
+
+        //add elements to the linkedlist
+        test.add("20");
+        test. add("F");
+        test.add("E");
+
+        test.add(1, "A2");
+
+        //Original contents of the test
+        System.out.println("contents of test " + test);
 
     }
 }
