febb6d015adb1c6ddf1cd97ca6c6602638b594f3
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-14 14:37:39 +0000

    feature: add implementation of method subset of interface navigable for treeset

--- chapt20/SDemoTreeTest.java
+++ chapt20/SDemoTreeTest.java
@@ -11,12 +11,21 @@ public class SDemoTreeTest {
    public static void main(String[] args) {
      TreeSet<String> hold = new TreeSet<String>();
 
+    hold.add("A");
+    hold.add("B");
+    hold.add("C");
+    hold.add("D");
+    hold.add("E");
     hold.add("F");
     hold.add("A");
     hold.add("5");
     hold.add("a");
 
     System.out.println("Display the contents " + hold);
+
+
+    //it implements the navigable interface that has method subSet
+    System.out.println(hold.subSet("B", "F"));
    }
 
     
