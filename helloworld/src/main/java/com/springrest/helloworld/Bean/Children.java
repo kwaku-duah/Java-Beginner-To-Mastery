67a2d14f31b0d023905a94e3183c1e32ac490300
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-16 16:46:37 +0000

    feature: add a student controller that creates a student object

--- helloworld/src/main/java/com/springrest/helloworld/Bean/Children.java
+++ helloworld/src/main/java/com/springrest/helloworld/Bean/Children.java
@@ -1,5 +1,9 @@
 package com.springrest.helloworld.Bean;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /* 
 /**
  * Efficiently enhances object creation by eliminating boilerplate code
@@ -8,9 +12,15 @@ package com.springrest.helloworld.Bean;
  * - @Data generates getters, setters, toString, equals, and hashCode.
  * - @AllArgsConstructor generates a constructor with all fields as parameters.
  * - @NoArgsConstructor generates a default constructor with no parameters.
+ * 
+ * all this can be achieved by adding lombok dependency
  */
 
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Children {
-    
+    private String fullName;
+    private int age;
+    private double height;
 }
---
+++ helloworld/src/main/java/com/springrest/helloworld/controller/StudentController.java
@@ -0,0 +1,15 @@
+package com.springrest.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springrest.helloworld.Bean.Student;
+
+@RestController
+public class StudentController {
+    
+    @GetMapping("/student-name")
+    public Student studentName() {
+        return new Student("Kwaku", "Duah");
+    }
+}
