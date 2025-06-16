bec3ff0091ba78da0bb17e945108fdf73ab39516
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-16 16:18:47 +0000

    feature: add a student object with manual constructors, getters and setters

---
+++ helloworld/src/main/java/com/springrest/helloworld/Bean/Student.java
@@ -0,0 +1,32 @@
+package com.springrest.helloworld.Bean;
+
+/*
+ * Build a springboot REST API that returns a Java bean
+ * A bean is simply an object that is managed by spring
+ */
+
+public class Student {
+    private String firstName;
+    private String lastName;
+
+    public Student(String first, String last) {
+        this.firstName = first;
+        this.lastName = last;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstname) {
+        this.firstName = firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastname) {
+        this.lastName = lastname;
+    }
+}
