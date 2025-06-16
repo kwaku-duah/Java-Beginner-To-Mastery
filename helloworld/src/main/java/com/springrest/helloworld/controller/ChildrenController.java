4a54aea1754226c968ac75f08487c6fd3151a142
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-16 16:59:31 +0000

    feature: add children class and controller to create a rest api

---
+++ helloworld/src/main/java/com/springrest/helloworld/controller/ChildrenController.java
@@ -0,0 +1,20 @@
+package com.springrest.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springrest.helloworld.Bean.Children;
+
+/*
+ * this class was created using lomboks annotation simple, easy and clean
+ * 
+ */
+
+ @RestController
+public class ChildrenController {
+    
+    @GetMapping("/child-info")
+    public Children childDetails(){
+        return new Children("Kwaku Duah",25, 170.24 );
+    }
+}
