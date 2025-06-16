42a416f40d25bd86d93d33a0b59ef1e67552deb6
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-16 15:47:16 +0000

    docs: add brief docs on springbootapplication annotation

--- helloworld/src/main/java/com/springrest/helloworld/HelloworldApplication.java
+++ helloworld/src/main/java/com/springrest/helloworld/HelloworldApplication.java
@@ -3,6 +3,17 @@ package com.springrest.helloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * @springbootapplication tells spring to do autoconfiguration
+ * it is the same as using all these three annotations together
+ * 
+ * @componentscan that tells spring to look for @controller, @ service etc
+ * @enableautoconfiguration tells spring to be alert and help in auto-figure 
+ * things based on libraries in this project
+ * 
+ * @configuration tells spring this contains beans *java objects* it should create
+ * and manage them
+ */
 @SpringBootApplication
 public class HelloworldApplication {
 
