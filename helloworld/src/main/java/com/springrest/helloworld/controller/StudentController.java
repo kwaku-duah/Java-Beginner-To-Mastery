ab2aa0792ba995cd4116160bd864aeb1bcaae32f
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-16 20:13:03 +0000

    feature: add rest api that uses a path variable annotation

--- helloworld/src/main/java/com/springrest/helloworld/controller/StudentController.java
+++ helloworld/src/main/java/com/springrest/helloworld/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.springrest.helloworld.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springrest.helloworld.Model.Student;
@@ -11,5 +12,29 @@ public class StudentController {
     @GetMapping("/student-name")
     public Student studentName() {
         return new Student("Kwaku", "Duah");
+    }
+
+    /*
+     * @pathvariable annotation lets spring grab something from
+     * a url and apply it to a method
+     * 
+     * subtle note: if the name in the url matches exactly as the method parameter
+     * name , you can skip putting the names in the paths
+     * 
+     * for example if pathvariable was first_name != firstName, then you must
+     * specify
+     * but since pathvariable is same as firstName and lastName, you can skip it in
+     * the paths
+     * 
+     */
+
+    @GetMapping("/student/{firstName}/{lastName}")
+
+    /*
+     * the @pathvariable("firstName") and ...."lastName" are unnecessary, used
+     * here for demonstration
+     */
+    public Student getInPath(@PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName) {
+        return new Student(firstName, lastName);
     }
 }
