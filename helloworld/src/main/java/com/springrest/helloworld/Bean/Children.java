6cf4adfad660e137d9cd0da615462d275850234e
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-16 16:31:09 +0000

    docs: add brief docs on lombok annotations

---
+++ helloworld/src/main/java/com/springrest/helloworld/Bean/Children.java
@@ -0,0 +1,16 @@
+package com.springrest.helloworld.Bean;
+
+/* 
+/**
+ * Efficiently enhances object creation by eliminating boilerplate code
+ * such as getters, setters, and constructors using Lombok annotations.
+ *
+ * - @Data generates getters, setters, toString, equals, and hashCode.
+ * - @AllArgsConstructor generates a constructor with all fields as parameters.
+ * - @NoArgsConstructor generates a default constructor with no parameters.
+ */
+
+
+public class Children {
+    
+}
