ffbe4fb8b4dc37e8211c746354018d7bc3049089
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-16 20:44:19 +0000

    feature: add implementation of a request that uses a @requestparam annotation to guide a method

--- helloworld/src/main/java/com/springrest/helloworld/controller/StudentController.java
+++ helloworld/src/main/java/com/springrest/helloworld/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.springrest.helloworld.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springrest.helloworld.Model.Student;
@@ -35,6 +36,20 @@ public class StudentController {
      * here for demonstration
      */
     public Student getInPath(@PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName) {
+        return new Student(firstName, lastName);
+    }
+
+    @GetMapping("/student/query")
+    public Student getRequest(
+
+    /*request params gives a tip in the url which guides a method in
+     * carrying out a task. The specification of name = firstName is 
+     * unnecessary and redundant because the method parameter and the name
+     * parameter are the same, 
+     * Left it here for basic reasons.
+     */
+            @RequestParam(name = "firstName") String firstName,
+            @RequestParam(name = "lastName") String lastName) {
         return new Student(firstName, lastName);
     }
 }
