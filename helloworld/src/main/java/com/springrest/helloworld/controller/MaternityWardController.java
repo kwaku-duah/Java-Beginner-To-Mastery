c5e45bec679f80362773179841184587b65cf9f6
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-16 20:22:20 +0000

    feature: add demonstration of efficient usage of pathvariable annotation that is not redundant

--- helloworld/src/main/java/com/springrest/helloworld/controller/MaternityWardController.java
+++ helloworld/src/main/java/com/springrest/helloworld/controller/MaternityWardController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import com.springrest.helloworld.Model.Children;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /*
  * this controller models the children class, newborn babies
@@ -14,20 +15,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class MaternityWardController {
-    
 
     @GetMapping("/all-children")
     public List<Children> getChildrenDetails() {
         List<Children> children = new LinkedList<>();
 
-        children.add(new Children("Kwaku Duah", 25, 166.45 ));
-        children.add(new Children("Gennaro Gattuso", 29, 196.45 ));
-        children.add(new Children("Ribery Frank", 75, 156.45 ));
-        children.add(new Children("Degraft Osei Owusu", 21, 146.45 ));
-        children.add(new Children("Osei Burak", 76, 30.45 ));
-        children.add(new Children("Christ Somiah", 30, 162.95 ));
-        children.add(new Children("Kenneth Benjamin", 45, 116.45 ));
+        children.add(new Children("Kwaku Duah", 25, 166.45));
+        children.add(new Children("Gennaro Gattuso", 29, 196.45));
+        children.add(new Children("Ribery Frank", 75, 156.45));
+        children.add(new Children("Degraft Osei Owusu", 21, 146.45));
+        children.add(new Children("Osei Burak", 76, 30.45));
+        children.add(new Children("Christ Somiah", 30, 162.95));
+        children.add(new Children("Kenneth Benjamin", 45, 116.45));
 
         return children;
+    }
+
+    /*
+     * Demonstration of efficient form of usage of @pathvariable that is not
+     * redundant
+     * 
+     */
+    @GetMapping("/children/{fullName}/{age}/{height}")
+    public Children getChild(@PathVariable String fullName, @PathVariable int age, @PathVariable double height) {
+        return new Children(fullName, age, height);
     }
 }
