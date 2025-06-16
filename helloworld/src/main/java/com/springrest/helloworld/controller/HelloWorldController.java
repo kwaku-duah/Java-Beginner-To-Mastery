528c78e65fd9e3e0723f5631e1c17bcff75d453d
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-16 15:32:36 +0000

    feature: add a get rest api endpoint with getmapping annotation

--- helloworld/src/main/java/com/springrest/helloworld/controller/HelloWorldController.java
+++ helloworld/src/main/java/com/springrest/helloworld/controller/HelloWorldController.java
@@ -1,10 +1,21 @@
 package com.springrest.helloworld.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController //restcontroller = @controller + @responsebody annotations
+
+@RestController //@restcontroller = @controller + @responsebody annotations
 public class HelloWorldController {
-    
+    /*@RestController tells spring, this is a web controller class
+    simply return values all methods under this return in the HTML response
+     * 
+     * 
+     * @GetMapping, tells spring, whenever the value in the annotation is matched, it
+     * should return whatever that the method returns
+     */
+
+
+    @GetMapping("/helloworld") //@alias for requestmapping.
     public String helloWorld() {
         return "Hello World";
     }
