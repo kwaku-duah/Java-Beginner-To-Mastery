7d87ec1969494ea4a764b28061c3e8ccf742cd2f
Sheikhman_Kalderon <duah229@outlook.com>
2025-06-17 09:18:09 +0000

    feature: add an update endpoint that uses put mapping to partially update existing data

--- springcrud/src/main/java/com/practice/springcrud/Controller/UserController.java
+++ springcrud/src/main/java/com/practice/springcrud/Controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,8 +38,8 @@ public class UserController {
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
 
-    @GetMapping("{id}")
-    public ResponseEntity<User> getUserById(@PathVariable("id") Long userId) {
+    @GetMapping("{userId}") //cleanly using same name as method parameter to avoid putting a value in pathvariable
+    public ResponseEntity<User> getUserById(@PathVariable() Long userId) {
         User fetchUser = userService.getUserById(userId);
         return new ResponseEntity<>(fetchUser, HttpStatus.OK);
     }
@@ -47,5 +48,14 @@ public class UserController {
     public ResponseEntity<List<User>> fetchAllUsers() {
         List<User> allUsers = userService.getAllUsers();
         return new ResponseEntity<>(allUsers, HttpStatus.OK);
+    }
+
+    @PutMapping("{userId}")
+    public ResponseEntity<User> updateUser(
+            @PathVariable() Long userId,
+            @RequestBody User user) {
+        user.setId(userId);
+        User updatedUser = userService.updateUser(user);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
     }
 }
